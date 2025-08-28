184b718926d7738f06e263da7aa2e4575206e15e
kushgarg016 <kushgarg016@gmail.com>
2025-08-29 00:51:14 +0530

    Update C6.c

--- C6.c
+++ C6.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main(){
     int i , sum=0;
-    for ( int i = 0; i <= 10 ; i++)
+    for ( int i = 0; i<= 10 ; i++)
     {
       sum = sum + i ;  
     }
-    printf("%d",sum); 
+    printf("%d\n",sum); 
     
     return 0; 
 } 
